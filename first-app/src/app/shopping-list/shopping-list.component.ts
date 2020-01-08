3283eb34f4c6d4dd20550bcad83624cd31fbd8c3
senthilsivaraman <senthilkumar150393@gmail.com>
2020-01-08 14:50:37 -0600

    Reipe Detail and Ingrdients model and component added

--- first-app/src/app/shopping-list/shopping-list.component.ts
+++ first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients : Ingredient[] =[
+    new Ingredient('Chicken',2),
+    new Ingredient('Onion', 5),
+    new Ingredient('Tomato', 4),
+    new Ingredient('Curry Leaves', 1),
+    new Ingredient('Coriander', 2),
+    new Ingredient('Spices', 4)
+  ];
 
   constructor() { }
 
