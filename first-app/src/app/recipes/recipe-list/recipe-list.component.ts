da192282546864cf2a6ab14da043ccfb20cb112b
senthilsivaraman <senthilkumar150393@gmail.com>
2020-01-08 01:14:45 -0600

    Added first Recipe list and description

--- first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [new Recipe('Mutton Kuzhambu',
+                      'Curry made with delicious goat meat marinated with spices and Indian Masala tossed in oil with onions, cashew and green chilly',
+                       'https://yummyindiankitchen.com/wp-content/uploads/2019/09/mutton-kulambu-in-tamil-style-mutton-kuzhambu.jpg' ),
+                       new Recipe('Mutton Kuzhambu',
+                      'Curry made with delicious goat meat marinated with spices and Indian Masala tossed in oil with onions, cashew and green chilly',
+                       'https://yummyindiankitchen.com/wp-content/uploads/2019/09/mutton-kulambu-in-tamil-style-mutton-kuzhambu.jpg' )
+                      ];
+
 
   constructor() { }
 
